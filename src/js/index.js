265c73da3e1b7517ad3f283788d5a388c65ff865
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-05-29 03:00:41 +0200

    create functions opening and closing panels

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 import '../scss/main.scss';
+const panelScores = document.querySelector('.panel--js');
+const panelScoresBtn = document.querySelector('.panel__btn--js');
+const panelLevels = document.querySelector('.levels--js');
+const panelLevelsBtn = document.querySelector('.levels__text--js');
+
+const openPanelScores = () => {
+    panelScores.classList.toggle('panel--hide');
+};
+
+const openPanelLevels = () => {
+    panelLevels.classList.toggle('levels--hide');
+};
+
+panelScoresBtn.addEventListener('click', openPanelScores);
+panelLevelsBtn.addEventListener('click', openPanelLevels);
